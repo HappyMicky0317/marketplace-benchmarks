45b79f36f0563eeb9e9efadb988112b819756c09
Brian0317 <onedev975@gmail.com>
2023-02-22 23:34:20 +0300

    3

--- src/marketplaces/sudoswap/interfaces/IPairFactory.sol
+++ src/marketplaces/sudoswap/interfaces/IPairFactory.sol
@@ -32,7 +32,7 @@ interface IPairFactory {
         uint256[] initialNFTIDs;
         uint256 initialTokenBalance;
     }
-// create ERC20 sdfgdg
+// create ERC20 sdfgd
 
     function createPairERC20(CreateERC20PairParams calldata params)
         external
